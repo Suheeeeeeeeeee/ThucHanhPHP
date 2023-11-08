beace2ac559a430746ee9bd9fb2bd5edd26f96e1
Suheeeeeeeeeee <121279919+Suheeeeeeeeeee@users.noreply.github.com>
2023-11-08 20:53:08 +0700

    bai15

---
+++ Bai15/config.php
@@ -0,0 +1,10 @@
+<?php
+$config = mysqli_connect('localhost:3306','root','','review');
+
+if(mysqli_connect_errno()!==0)
+{
+die("Error: Could not connect to the database. An error ".mysqli_connect_error()."
+ocurred.");
+}
+mysqli_set_charset($config,'utf8');
+?>
